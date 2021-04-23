abade1e6c2251b6a5917f6b927718f2fcc17d7d0
luke-foster <lukefoster2011@gmail.com>
2021-04-23 20:59:41 +0100

    made modal popup on description area only

--- group3-help-queue-fe/src/queuedTickets.jsx
+++ group3-help-queue-fe/src/queuedTickets.jsx
@@ -10,6 +10,7 @@ const QueuedTickets = () => {
 
     const openTicketModal = () => {
         setShowTicketModal(prev => !prev);
+        <CardModal showTicketModal={showTicketModal} setShowTicketModal={setShowTicketModal}/>
     }
 
 
@@ -39,9 +40,9 @@ const QueuedTickets = () => {
 			</button>
             {
                 tickets.map((cardStuff) => (
-                    <div className="card" onClick={openTicketModal}>
+                    <div className="card">
                         <div className="ticket-user-time"><h3 className="inline">{cardStuff.usersName}</h3>      <p className="inline">{cardStuff.time}</p></div>
-                        <div className="ticket-description"><h3>{cardStuff.ticketTitle}</h3><p>{cardStuff.ticketDescription}</p></div>
+                        <div className="ticket-description" onClick={openTicketModal}><h3>{cardStuff.ticketTitle}</h3><p>{cardStuff.ticketDescription}</p></div>
                         <div className="complete-button">
                         <button className="btn" id="ticket-complete">Completed</button>
                         <button className="btn" id="ticket-edit">Edit</button>
