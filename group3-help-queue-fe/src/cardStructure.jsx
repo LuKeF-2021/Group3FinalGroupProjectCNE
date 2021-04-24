67b53de2be3645f584ddcf057c4aea6826ea9020
luke-foster <lukefoster2011@gmail.com>
2021-04-24 13:52:59 +0100

    added icons for cards

--- group3-help-queue-fe/src/cardStructure.jsx
+++ group3-help-queue-fe/src/cardStructure.jsx
@@ -1,14 +1,22 @@
+import { HiCheck } from "react-icons/hi";
+import { HiPencil } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
+import { HiOutlineArrowsExpand } from "react-icons/hi";
+
 
 const CardStructure = ({cardStuff , openTicketModal}) => {
     return (
         <div className="column" key={cardStuff.id}>
             <div className="card">
-                <div className="ticket-user-time"><h3 className="inline">{cardStuff.usersName}</h3>      <p className="inline">{cardStuff.time}</p></div>
-                <div className="ticket-description" onClick={openTicketModal}><h3>{cardStuff.ticketTitle}</h3><p>{cardStuff.ticketDescription}</p></div>
-                <div className="complete-button">
-                    <button className="btn" id="ticket-complete">Completed</button>
+                <div className="ticket-user-time"><h3 className="inline">{cardStuff.usersName}</h3>      <p className="inline">{cardStuff.time}</p> <HiOutlineArrowsExpand style={{ verticalAlign: 'middle' }} size={28} className="expand" onClick={() => openTicketModal(cardStuff)}/></div>
+                <div className="ticket-description"><h3>{cardStuff.ticketTitle}</h3><p>{cardStuff.ticketDescription}</p></div>
+                <div className="card-actions">
+                    {/* <button className="btn" id="ticket-complete">Completed</button>
                     <button className="btn" id="ticket-edit">Edit</button>
-                    <button className="btn" id="ticket-delete">Delete</button>
+                    <button className="btn" id="ticket-delete">Delete</button> */}
+                    <HiCheck style={{ verticalAlign: 'middle' }} size={28} className="check"/>
+                    <HiPencil style={{ verticalAlign: 'middle' }} size={28}/>
+                    <HiX style={{ verticalAlign: 'middle' }} size={28} className="cross"/>
                 </div>
             </div>
         </div>
--- group3-help-queue-fe/src/queuedTickets.jsx
+++ group3-help-queue-fe/src/queuedTickets.jsx
@@ -6,32 +6,14 @@ import './Tickets.css';
 const QueuedTickets = ({tickets}) => {
 
     const [showTicketModal, setShowTicketModal] = useState(false);
-    // const [allTickets, setAllTickets] = useState([]);
 
     const openTicketModal = (id) => {
         setShowTicketModal(prev => !prev);
-        console.log(id);
+        console.log('opened', id);
     }
 
 
-    // const [tickets, setTickets] = useState([
-    // {
-
-    //     id:1,
-    //     usersName: "Luke Foster",
-    //     time: "17:14",
-    //     ticketDescription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error perferendis porro consequuntur illum omnis quas ex incidunt eius eos sed magni earum iusto maiores laboriosam numquam maxime, quidem nobis sint.",
-    //     ticketTitle: "Docker Problem"
-
-    // },
-    // {
-    //     id:2,
-    //     usersName: "Jack Smith",
-    //     time: "17:36",
-    //     ticketDescription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error perferendis porro consequuntur illum omnis quas ex incidunt eius eos sed magni earum iusto maiores laboriosam numquam maxime, quidem nobis sint.",
-    //     ticketTitle: "Terraform Problem"
-    // }]
-    // )
+    
 
     return (
         <div className="queuedHeading">
@@ -41,17 +23,6 @@ const QueuedTickets = ({tickets}) => {
 			</button>
             {
                 tickets.map((cardStuff) => (
-                    // <div className="column" key={cardStuff.id}>
-                    // <div className="card">
-                    //     <div className="ticket-user-time"><h3 className="inline">{cardStuff.usersName}</h3>      <p className="inline">{cardStuff.time}</p></div>
-                    //     <div className="ticket-description" onClick={() => openTicketModal(tickets.id)}><h3>{cardStuff.ticketTitle}</h3><p>{cardStuff.ticketDescription}</p></div>
-                    //     <div className="complete-button">
-                    //     <button className="btn" id="ticket-complete">Completed</button>
-                    //     <button className="btn" id="ticket-edit">Edit</button>
-                    //     <button className="btn" id="ticket-delete">Delete</button>
-                    //     </div>
-                    // </div>
-                    // </div>
                     <CardStructure key={cardStuff.id} cardStuff={cardStuff} openTicketModal={openTicketModal}/>
                 ))
                 
