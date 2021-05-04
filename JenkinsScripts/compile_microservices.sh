086272d9d75fb3c4d193fa64724b68986ea5d9b5
DylanRitchings <Bigbird167->
2021-05-04 22:12:39 +0100

    Quick fix

--- JenkinsScripts/compile_microservices.sh
+++ JenkinsScripts/compile_microservices.sh
@@ -1,22 +1,39 @@
-#Run disco server
-
-cd Microservices/CreateTicketService
-mvn test
-
-#Download dependencies?
-
-mvn dependency:tree
-
-#Compile
-#mvn compile
-
-#Compile and run?
-#mvn spring-boot:run
-
-#Build/compile?
-mvn clean package
-
-
 #Run compiled package
 
-java -jar name.jar --spring.datasource.url=${db_uri} --spring.datasource.username=${db_user} --spring.datasource.pass=${db_pass}
+#java -jar name.jar --spring.datasource.url=${db_uri} --spring.datasource.username=${db_user} --spring.datasource.pass=${db_pass}
+
+
+cd Microservices/Disco-Server
+mvn clean package
+cd target
+java -jar Disco-Server-0.0.1-SNAPSHOT.jar
+
+cd ../../CreateTicketService
+mvn clean package
+cd target
+java -jar CreateTicketService-0.0.1-SNAPSHOT.jar
+
+cd ../../DeleteTicketService
+mvn clean package
+cd target
+java -jar DeleteTicketService-0.0.1-SNAPSHOT.jar
+
+cd ../../ReadAllTicketsService
+mvn clean package
+cd target
+java -jar ReadAllTicketsService-0.0.1-SNAPSHOT.jar
+
+cd ../../ReadTicketService
+mvn clean package
+cd target
+java -jar ReadTicketService-0.0.1-SNAPSHOT.jar
+
+cd ../../UpdateTicketService
+mvn clean package
+cd target
+java -jar UpdateTicketService-0.0.1-SNAPSHOT.jar
+
+cd ../../TicketGateway
+mvn clean package
+cd target
+java -jar TicketGateway-0.0.1-SNAPSHOT.jar
