4f76c69009a5d745662236daa4eb2c90f17cbd81
zonaira-hussain <zonaira@hotmail.co.uk>
2021-05-05 10:32:51 +0000

    test

--- jenkins/buildDockerContainers.sh
+++ jenkins/buildDockerContainers.sh
@@ -5,11 +5,11 @@
 sudo docker login -u $USERNAME -p $PASSWORD
 sudo docker network create project-network
 sudo docker run -d --network project-network -p 3000:3000 --name lukef2021/frontend:latest 
-sudo docker run -d --network project-network -p 8904:8904 --name lukef2021/readTicketService:latest
-sudo docker run -d --network project-network -p 8903:8903 --name lukef2021/readAllTicketService:latest
-sudo docker run -d --network project-network -p 8901:8901 --name lukef2021/createTicketService:latest
-sudo docker run -d --network project-network -p 8905:8905 --name lukef2021/updateTicketService:latest
-sudo docker run -d --network project-network -p 8902:8902 --name lukef2021/deleteTicketService:latest
+sudo docker run -d --network project-network -p 8904:8904 --name lukef2021/readticketservice:latest
+sudo docker run -d --network project-network -p 8903:8903 --name lukef2021/readallticketservice:latest
+sudo docker run -d --network project-network -p 8901:8901 --name lukef2021/createticketservice:latest
+sudo docker run -d --network project-network -p 8905:8905 --name lukef2021/updateticketservice:latest
+sudo docker run -d --network project-network -p 8902:8902 --name lukef2021/deleteticketservice:latest
 sudo docker run -d --network project-network -p 8900:8900 --name lukef2021/discovery-server:latest
 sudo docker run -d --network project-network -p 80:8899 --name lukef2021/ticket-gateway:latest
 # EOF
--- jenkins/buildDockerImages.sh
+++ jenkins/buildDockerImages.sh
@@ -15,33 +15,33 @@ cd ..
 
 # finding backend/Dockerfile
 echo "build backend-create images started"
-cd backend
+cd DockerfilesForApp
 cd create
-sudo docker build -t createTicketService:latest .
+sudo docker build -t createticketservice:latest .
 cd ..
 
 echo "build backend-delete images started"
 
 cd delete
-sudo docker build -t deleteTicketService:latest .
+sudo docker build -t deleteticketservice:latest .
 cd ..
 
 echo "build backend-read images started"
 
 cd read
-sudo docker build -t readTicketService:latest .
+sudo docker build -t readticketservice:latest .
 cd ..
 
 echo "build backend-readAll images started"
 
 cd readAll
-sudo docker build -t readAllTicketService:latest .
+sudo docker build -t readallticketservice:latest .
 cd ..
 
 echo "build backend-update images started"
 
 cd update
-sudo docker build -t updateTicketService:latest .
+sudo docker build -t updateticketservice:latest .
 cd ..
 
 echo "build backend-discoServer images started"
@@ -62,21 +62,21 @@ cd ..
 echo "Pushing images to DockerHub"
 
 # sh './logindocker.sh'
-sudo docker tag readTicketService:latest lukef2021/readTicketService:latest
-sudo docker tag createTicketService:latest lukef2021/createTicketService:latest
-sudo docker tag updateTicketService:latest lukef2021/updateTicketService:latest
-sudo docker tag readAllTicketService:latest lukef2021/readAllTicketService:latest
-sudo docker tag deleteTicketService:latest lukef2021/deleteTicketService:latest
+sudo docker tag readticketservice:latest lukef2021/readticketservice:latest
+sudo docker tag createticketservice:latest lukef2021/createticketservice:latest
+sudo docker tag updateticketservice:latest lukef2021/updateticketservice:latest
+sudo docker tag readallticketservice:latest lukef2021/readallticketservice:latest
+sudo docker tag deleteticketservice:latest lukef2021/deleteticketservice:latest
 sudo docker tag ticket-gateway:latest lukef2021/ticket-gateway:latest
 sudo docker tag discover-server:latest lukef2021/discovery-server:latest
 sudo docker tag frontend:latest lukef2021/frontend:latest
 sudo docker login -u $USERNAME -p $PASSWORD
 sudo docker push lukef2021/frontend:latest
-sudo docker push lukef2021/readTicketService:latest
-sudo docker push lukef2021/createTicketService:latest
-sudo docker push lukef2021/readAllTicketService:latest
-sudo docker push lukef2021/updateTicketService:latest
-sudo docker push lukef2021/deleteTicketService:latest
+sudo docker push lukef2021/readticketservice:latest
+sudo docker push lukef2021/createticketservice:latest
+sudo docker push lukef2021/readallticketservice:latest
+sudo docker push lukef2021/updateticketservice:latest
+sudo docker push lukef2021/deleteticketservice:latest
 sudo docker push lukef2021/ticket-gateway:latest
 sudo docker push lukef2021/discovery-server:latest
 
