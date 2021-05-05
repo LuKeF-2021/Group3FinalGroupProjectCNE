e9d1d75807d8e30c3c0a49af6bdfb118493c5b76
DylanRitchings <Bigbird167->
2021-05-05 15:35:20 +0100

    More testing changes

--- Microservices/ReadTicketService/src/test/java/com/qa/readticketservice/ReadTicketServiceApplicationTests.java
+++ Microservices/ReadTicketService/src/test/java/com/qa/readticketservice/ReadTicketServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.qa.readticketservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class ReadTicketServiceApplicationTests {
 
     @Test
--- Microservices/TicketGateway/src/test/java/com/qa/ticketgateway/service/TicketGatewayServiceIntegrationTest.java
+++ Microservices/TicketGateway/src/test/java/com/qa/ticketgateway/service/TicketGatewayServiceIntegrationTest.java
@@ -1,4 +1,7 @@
 package com.qa.ticketgateway.service;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(profiles = "test")
 public class TicketGatewayServiceIntegrationTest {
 }
