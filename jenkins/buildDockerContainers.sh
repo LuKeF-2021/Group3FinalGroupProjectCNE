c53219bd1af89d6723ece9001a837330912f2ce0
zonaira-hussain <zonaira@hotmail.co.uk>
2021-05-05 14:08:04 +0000

    changes

--- jenkins/buildDockerContainers.sh
+++ jenkins/buildDockerContainers.sh
@@ -3,13 +3,14 @@
 # ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@34.240.61.86 << EOF
 # sh './logindocker.sh'
 sudo docker login -u $USERNAME -p $PASSWORD
-sudo docker network create project-network
-sudo docker run -d --network project-network -p 3000:3000 --name lukef2021/frontend:latest
-sudo docker run -d --network project-network -p 8904:8904 --name lukef2021/readticketservice:latest
-sudo docker run -d --network project-network -p 8903:8903 --name lukef2021/readallticketservice:latest
-sudo docker run -d --network project-network -p 8901:8901 --name lukef2021/createticketservice:latest
-sudo docker run -d --network project-network -p 8905:8905 --name lukef2021/updateticketservice:latest
-sudo docker run -d --network project-network -p 8902:8902 --name lukef2021/deleteticketservice:latest
-sudo docker run -d --network project-network -p 8900:8900 --name lukef2021/discovery-server:latest
-sudo docker run -d --network project-network -p 80:8899 --name lukef2021/ticket-gateway:latest
+# sudo docker network create project-network
+sudo docker run discovery-server:latest
+sudo docker run ticket-gateway:latest
+sudo docker run readticketservice:latest
+sudo docker run readallticketservice:latest
+sudo docker run createticketservice:latest
+sudo docker run updateticketservice:latest
+sudo docker run deleteticketservice:latest
+# sudo docker run frontend:latest
+
 # EOF
