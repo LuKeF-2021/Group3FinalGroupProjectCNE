71c02bd027aeb7a0504bd486aca9d460dedc3352
Ubuntu <ubuntu@ip-172-31-1-114.eu-west-1.compute.internal>
2021-05-06 13:55:37 +0000

    docker-compose changes

--- jenkins/kubernetes.sh
+++ jenkins/kubernetes.sh
@@ -13,19 +13,19 @@ kubectl create -f frontend_service.yml
 cd ..
 cd backend/ 
 kubectl create -f backend_service.yml
-# cd ..
-# cd kubernetes/nginx/
-# kubectl create -f nginxlb.yml
+cd ..
+cd nginx/
+kubectl create -f nginxlb.yml
 
-# echo 'creating nginx config'
-# cd ..
-# cd kubernetes/nginx/
-# kubectl apply -f nginx_config.yml
+echo 'creating nginx config'
+cd ..
+cd nginx/
+kubectl apply -f nginx_config.yml
 
 echo 'creating deployment files'
-# cd ..
-# cd kubernetes/nginx/
-# kubectl apply -f nginx.yml
+cd ..
+cd nginx/
+kubectl apply -f nginx.yml
 cd ..
 cd frontend/
 kubectl apply -f frontend_deploy.yml
